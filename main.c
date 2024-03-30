de9ae473147a900e935bdd776f10b9b8cf64a197
drimag <rafaelvedimagiba@gmail.com>
2024-03-31 01:07:20 +0800

    cleaned up code

--- main.c
+++ main.c
@@ -12,74 +12,44 @@ double generate_random_double() {
 	return random_double;
 }
 
+void run_dot_product(n) {
+
+	srand(time(NULL));
+	printf("Running for: %d\n", n);
+	double sdot = 0;
+
+	double* A = (double*)malloc(n * sizeof(double));
+	double* B = (double*)malloc(n * sizeof(double));
+
+	for (int i = 0; i < n; i++) {
+		A[i] = generate_random_double();
+		B[i] = generate_random_double();
+	}
+	
+	sdot = dot_prod_c(A, B, n);
+	printf("C - Dot Product: %lf\n", sdot);
+
+	sdot = dot_prod_asm(A, B, n);
+	printf("ASM - Dot Product: %lf\n", sdot);
+
+	free(A);
+	free(B);
+}
+
 int main() {
 	double A[] = { 2.5, 3, 4.5 };
 	double B[] = { 1, 1, 1 };
 	long long int n = 3;
 	double sdot = 0; //for this, sdot should be equal to 10 (2.5*1+3*1+4.5*1=10)
 
-	srand(time(NULL));
-
-
-
 	// 1048576 = 2^20
-	double* A20 = (double*)malloc(1048576 * sizeof(double));
-	double* B20 = (double*)malloc(1048576 * sizeof(double));
-
-	for (int i = 0; i < 1048576; i++) {
-		A20[i] = generate_random_double();
-		B20[i] = generate_random_double();
-	}
-
-	// run and time the algos here
-
-	free(A20);
-	free(B20);
-
-
-
+	run_dot_product(1048576);
 	// 16777216 = 2^24
-	double* A24 = (double*)malloc(16777216 * sizeof(double));
-	double* B24 = (double*)malloc(16777216 * sizeof(double));
-
-	for (int i = 0; i < 16777216; i++) {
-		A24[i] = generate_random_double();
-		B24[i] = generate_random_double();
-	}
-
-	// run and time the algos here
-
-	free(A24);
-	free(B24);
-
-
-
+	run_dot_product(16777216);
 	// 1073741824 = 2^30
-	double* A30 = (double*)malloc(1073741824 * sizeof(double));
-	double* B30 = (double*)malloc(1073741824 * sizeof(double));
+	run_dot_product(1073741824);
 
-	for (int i = 0; i < 1073741824; i++) {
-		A30[i] = generate_random_double();
-		B30[i] = generate_random_double();
-	}
-
-	// run and time the algos here
-
-	printf("Vector A: ");
-	for (int i = 0; i < 1073741824; i++) {
-		printf("%lf ", A30[i]);
-	}
-	printf("\n");
-
-	free(A30);
-	free(B30);
-
-
-	sdot = dot_prod_c(A, B, n);
-	printf("C - Dot Product: %lf\n", sdot);
-
-	sdot = dot_prod_asm(A, B, n);
-	printf("ASM - Dot Product: %lf\n", sdot);
+	
 	return 0;
 }
 
