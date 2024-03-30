645d5fd90888ce66f6db1d638642a6b3ab80ee23
Sarah Jumilla <sarahericka.jumilla@gmail.com>
2024-03-31 00:11:01 +0800

    Add dot prod C file

---
+++ cfunc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+static double dot_prod_c(double A[], double B[], long long int n){
+	double res = 0;
+	for (int i = 0; i < n; i++) {
+		res = res + (A[i] * B[i]);
+	}
+	return res;
+}
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include "cfunc.c"
 
 extern double dot_prod_asm(double A[], double B[], long long int n);
 
@@ -10,8 +11,10 @@ int main() {
 	long long int n = 3;
 	double sdot = 0; //for this, sdot should be equal to 10 (2.5*1+3*1+4.5*1=10)
 
+	sdot = dot_prod_c(A, B, n);
+	printf("C - Dot Product: %lf\n", sdot);
 
 	sdot = dot_prod_asm(A, B, n);
-	printf("Dot Product: %lf\n", sdot);
+	printf("ASM - Dot Product: %lf\n", sdot);
 	return 0;
 }
