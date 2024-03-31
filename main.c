53c8b3a7c72c2b922bb5bf9b71d1c69d6807a9f7
Sarah Jumilla <sarahericka.jumilla@gmail.com>
2024-03-31 23:11:04 +0800

    added 30 runs for each ver

--- main.c
+++ main.c
@@ -16,44 +16,59 @@ void run_dot_product(n) {
 
 	srand(time(NULL));
 	printf("Running for: %d\n", n);
-	double sdot = 0;
 
 	double* A = (double*)malloc(n * sizeof(double));
 	double* B = (double*)malloc(n * sizeof(double));
 
-	for (int i = 0; i < n; i++) {
+	for (long long int i = 0; i < n; i++) {
 		A[i] = generate_random_double();
 		B[i] = generate_random_double();
 	}
-	
-	clock_t start_c = clock();
-	sdot = dot_prod_c(A, B, n);
-	clock_t end_c = clock();
-	double time_taken_c = ((double)(end_c - start_c)) / CLOCKS_PER_SEC;
-	printf("C - Dot Product: %lf (Time: %lf seconds)\n", sdot, time_taken_c);
 
-	clock_t start_asm = clock();
-	sdot = dot_prod_asm(A, B, n);
-	clock_t end_asm = clock();
-	double time_taken_asm = ((double)(end_asm - start_asm)) / CLOCKS_PER_SEC;
-	printf("ASM - Dot Product: %lf (Time: %lf seconds)\n", sdot, time_taken_asm);
+	clock_t start_c, start_asm;
+	clock_t end_c, end_asm;
+
+	double time_taken_c, time_taken_asm;
+
+	double total_time_c = 0.0;
+	double total_time_asm = 0.0;
+
+	double c_sdot = 0.0;
+	double asm_sdot = 0.0;
+
+	for (long long int i = 0; i < 30; i++){//run 30 times
+		start_c = clock();
+		c_sdot = dot_prod_c(A, B, n);
+		end_c = clock();
+		time_taken_c = ((double)(end_c - start_c)) / CLOCKS_PER_SEC;
+		total_time_c += time_taken_c;
+
+
+		start_asm = clock();
+		asm_sdot = dot_prod_asm(A, B, n);
+		end_asm = clock();
+		time_taken_asm = ((double)(end_asm - start_asm)) / CLOCKS_PER_SEC;
+		total_time_asm += time_taken_asm;
+	}
+
+	double avg_time_c = total_time_c / 30;
+	double avg_time_asm = total_time_asm / 30;
+
+	printf("C - Dot Product: %lf (Time: %lf seconds)\n", c_sdot, avg_time_c);
+	printf("x86-64 ASM - Dot Product: %lf (Time: %lf seconds)\n\n", asm_sdot, avg_time_asm);
 
 	free(A);
 	free(B);
 }
 
 int main() {
-	double A[] = { 2.5, 3, 4.5 };
-	double B[] = { 1, 1, 1 };
-	long long int n = 3;
-	double sdot = 0; //for this, sdot should be equal to 10 (2.5*1+3*1+4.5*1=10)
-
 	// 1048576 = 2^20
 	run_dot_product(1048576);
 	// 16777216 = 2^24
 	run_dot_product(16777216);
 	// 1073741824 = 2^30
-	run_dot_product(1073741824);
+	// 134217728 = 2^27
+	run_dot_product(134217728);
 
 	
 	return 0;
